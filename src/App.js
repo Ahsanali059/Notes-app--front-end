38666506276e8bd3c8da2f8b80c68fd6983dcf4d
Ahsan ali <ahsandev.org@gmail.com>
2023-12-23 11:49:43 +0500

    added notes app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [newNotes, setNewNotes] = useState({ title: '', content: '' });
+
+  useEffect(() => {
+    axios.get('http://localhost:1999/api/notes')
+      .then(response => setNotes(response.data))
+      .catch(error => console.error('Error fetching notes: ', error));
+  }, []);
+
+  const addNotes = () => {
+    axios.post('http://localhost:1999/api/notes', newNotes)
+      .then(response => {
+        setNotes(prevNotes => [...prevNotes, response.data]);
+        setNewNotes({ title: '', content: '' });
+      })
+      .catch(error => console.error("Error occurred creating Notes ", error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notes app</h1>
+
+      <div>
+        <h2>My Notes</h2>
+
+        <ul>
+          {notes.map(note => (
+            <li key={note.id}>{note.title} - {note.content}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <input
+          type='text'
+          placeholder='title'
+          value={newNotes.title}
+          onChange={e => setNewNotes({ ...newNotes, title: e.target.value })}
+        />
+        <br />
+        <textarea
+          placeholder="Content"
+          value={newNotes.content}
+          onChange={e => setNewNotes({ ...newNotes, content: e.target.value })}
+        />
+        <br />
+        <button onClick={addNotes}>Add Note</button>
+      </div>
     </div>
   );
 }
